d53f52057601d589612998459615010962f0b22c
yy771693 <81515340+yy771693@users.noreply.github.com>
2021-03-28 21:14:46 +0800

    Add files via upload

---
+++ ConsoleExample2/MyClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleExample2
+{
+    class MyClass
+    {
+        /// <summary>
+        /// 定义加法运算
+        /// </summary>
+        /// <param name="x">加数</param>
+        /// <param name="y">加数</param>
+        /// <returns></returns>
+        public int JiaFa(int x, int y) {
+            int z;
+            z = x + y;
+            Console.WriteLine(z);
+            return z;
+        }
+        /// <summary>
+        /// 定义减法运算
+        /// </summary>
+        /// <param name="a">减数</param>
+        /// <param name="b">被减数</param>
+        /// <returns></returns>
+        public int JianFa(int a, int b) {
+            int c;
+            c = a - b;
+            Console.WriteLine(c);
+            return c;
+        }
+        /// <summary>
+        /// 定义乘法运算
+        /// </summary>
+        /// <param name="m">乘数</param>
+        /// <param name="n">乘数</param>
+        /// <returns></returns>
+        public int ChengFa(int m, int n) {
+            int p;
+            p = m * n;
+            Console.WriteLine(p);
+            return p;
+        }
+        /// <summary>
+        /// 定义除法运算
+        /// </summary>
+        /// <param name="h">被除数</param>
+        /// <param name="k">除数</param>
+        /// <returns></returns>
+        public int ChuFa(int h, int k) {
+            int j;
+            j = h / k;
+            Console.WriteLine(j);
+            return j;
+        }
+    }
+}
---
+++ ConsoleExample2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleExample2
+{
+    class Program
+    {
+        public static void Equals(int x,int y) {
+            if (x == y)
+                Console.WriteLine("两数相等！");
+            else
+                Console.WriteLine("两数不同！");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("欢迎使用此计算器！");
+            Console.WriteLine("***进行加法运算请输入1***");
+            Console.WriteLine("***进行减法运算请输入2***");
+            Console.WriteLine("***进行乘法运算请输入3***");
+            Console.WriteLine("***进行除法运算请输入4***");
+            Console.WriteLine("请先依次输入所要计算的两个数，并选择对应的运算方法：");
+            int x, y;
+            int i = 0;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            i =Convert.ToInt32(Console.ReadLine());
+            Equals(x, y);
+            MyClass MC = new MyClass();
+            try
+            {
+                if (i == 1)
+                    MC.JiaFa(x, y);
+                else if (i == 2)
+                    MC.JianFa(x, y);
+                else if (i == 3)
+                    MC.ChengFa(x, y);
+                else if (i == 4)
+                    MC.ChuFa(x, y);
+                else
+                    Console.WriteLine("功能选择输入有误！");
+            }
+            catch
+            {
+                Console.WriteLine("分母不可为0！计算错误！");
+            }
+            finally {
+                Console.WriteLine("计算结束！");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
